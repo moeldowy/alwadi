d063ea0d1fa5e0fe923f6889cc24eb95bbf20929
Mohammed Salah <moeldowy@iti.gov.eg>
2023-12-10 10:31:45 +0200

    first lesson

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-echo"Welcome To my Website";
+//declare variable
+/*$user="Mai";//value+data type
+//redeclare the same variable
+$user=15;
+
+
+echo "Hi, $user";
+echo"<br>";
+echo 'Hi, $user';
+echo"<h1>Welcome</h1>";
+echo'<h3>Welcome</h3>';
+
+//declare Constant
+define('FAV_CLUB','Alahly');
+// No Acceptable to be redeclared
+
+define('FAV_CLUB','zamalek');
+
+echo FAV_CLUB;*/
