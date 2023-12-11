88b8faa020983d2f355a07e04041c34f9dd6297d
Mohammed Salah Eldowy <moeldowy@iti.gov.eg>
2023-12-11 13:41:12 +0200

    ahmed mostafa Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "parts/nav.php";?>
 <div class="row">
     <div class="col-4">
         <form class="row g-3" method="post" action="home.php">
-
+        <h3 class="display-3">Please Sign in!</h3>
             <div class="col-12">
                 <label for="username" class="form-label">User Name</label>
                 <input type="text" class="form-control" id="username" name="user" required>
